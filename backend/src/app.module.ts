03fccec7edfec675e9338511c2093750f42a20de
Omar Naifar <omarneyfar@gmail.com>
2026-06-24 17:31:53 +0100

    feat: add exception logic

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,13 +1,21 @@
 import { Module } from '@nestjs/common';
+import { APP_FILTER } from '@nestjs/core';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
 import { ClientModule } from './modules/client/client.module';
 import { OpportunityModule } from './modules/opportunity/opportunity.module';
+import { PrismaExceptionFilter } from './common/filters/prisma-exception.filter';
 
 @Module({
   imports: [PrismaModule, ClientModule, OpportunityModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: APP_FILTER,
+      useClass: PrismaExceptionFilter,
+    },
+  ],
 })
 export class AppModule {}
